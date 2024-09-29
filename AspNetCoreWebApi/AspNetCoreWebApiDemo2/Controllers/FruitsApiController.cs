9e8763d9e043b5077e9057e39274073c3a49cd9f
UjjwalAbhishek123 <ujjwalabhishek15@gmail.com>
2024-09-29 12:37:36 +0530

    2nd Commit AspNetCore Web Api Demo2

---
+++ AspNetCoreWebApi/AspNetCoreWebApiDemo2/Controllers/FruitsApiController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebApiDemo2.Controllers
+{
+    //URL => api/FruitsApi
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsApiController : ControllerBase
+    {
+        //Creating List of fruits
+        public List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Banana",
+            "Mango",
+            "Grapes",
+            "Orange"
+        };
+
+        //Creating method to return Fruits => HttpGet Attribute
+        //GET: GetFruits() => api/FruitsApi
+
+        [HttpGet]
+        public List<string> GetFruits()
+        {
+            return fruits;
+        }
+
+        //creating method to return fruit data based on specific id
+        //GET: GetFruitById(int id) api/FruitsApi/2
+        //[HttpGet("{id}")]
+        //public string GetFruitById(int id)
+        //{
+        //    return fruits.ElementAt(id);
+        //}
+
+        [HttpGet("{id}")]
+        public IActionResult GetFruitById(int id)
+        {
+            try
+            {
+                // Check if the id is within the valid range
+                if (id < 0 || id >= fruits.Count)
+                {
+                    return NotFound(new { Message = $"Fruit with ID {id} not found." });
+                }
+
+                // Return the fruit if the id is valid
+                return Ok(fruits[id]);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "Internal Server Error", Details = ex.Message });
+            }
+        }
+
+    }
+}
